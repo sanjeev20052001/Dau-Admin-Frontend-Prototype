d3b4e2afcf9b3360d43e2aab4ad0b06b417f9461
sanjeev20052001 <sanjeev24433@gmail.com>
2023-02-07 21:35:13 +0530

    new start

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import HomeScreen from './screens/Homescreen';
+
+function App() {
+  return (
+    <div>
+      <HomeScreen />
+    </div>
+  );
+}
+export default App;
---
+++ src/screens/Homescreen.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomeScreen = () => {
+    return (
+        <div className="full-screen">
+            <div className="py-2">
+
+            </div>
+        </div>
+    );
+};
+
+export default HomeScreen;
